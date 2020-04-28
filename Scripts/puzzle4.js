b128cc1a4678eb4e8fa0244058b35e15086972dc
krishnachaugule <58576563+krishnachaugule@users.noreply.github.com>
2020-04-28 19:04:06 -0400

    added another repo

---
+++ Scripts/puzzle4.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+    function myAnswer4() {
+            var a = document.getElementById("A1").value;
+            if (a == 522) {
+        alert("Congratulations! You got that right!")
+    }
+            else {
+        alert("Don't Give Up!! Try Again!!")
+    }
+        }
+function myHint() {
+    //Each clock, calculator and bulb has its own significance that helps derive its value!
+    alert("Make sure to keep a track of time! I'm always crunching numbers so my calculator watch is a must! Each bulb gives a different shade of light!  ")
+}
---
+++ Scripts/puzzle5.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+    function myAnswer5() {
+            var a = document.getElementById("A1").value;
+            if (a == 366397) {
+        alert("Congratulations! You got that right!")
+    }
+            else {
+        alert("Don't Give Up!! Try Again!!")
+    }
+        }
+function myHint() {
+    alert("Worried about the last two digits? Try to get the last two digits from the first four digits in the answer.")
+}
